a1b3e396273edd58de33fdd313a4855eed54a5d9
Crane Chu <cranechu@localhost.localdomain>
2018-07-23 22:36:04 +0800

    add more dnf package

--- restore.sh
+++ restore.sh
@@ -1,14 +1,14 @@
 sudo dnf install -y redhat-rpm-config
 sudo dnf install -y python3-devel
 sudo dnf install -y emacs-nox
-sudo dnf install -y gnome-tweak-tool
+sudo dnf install -y gnome-tweak-tool fio nvme-cli util-linux-user
+sudo dnf update -y
 
 #dconf dump /org/gnome/ > gnome.dconf.bak
 dconf load /org/gnome/ < gnome.dconf.bak
 
 sudo dnf install -y zsh
 rm -rf ~/.oh-my-zsh
-cp .zshrc ~/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp .zshrc ~/
 
-sudo dnf update -y
