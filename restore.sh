c02ca3c6662144ba593ebf625d209d8859a13eb7
Crane Chu <cranechu@gmail.com>
2019-10-01 08:20:21 +0800

    clone history-search-multi-word by auto

--- restore.sh
+++ restore.sh
@@ -9,7 +9,7 @@ sudo dnf install -y redhat-rpm-config
 sudo dnf install -y python3-devel
 sudo dnf install -y emacs-nox
 sudo dnf install -y gnome-tweak-tool fio nvme-cli util-linux-user
-sudo dnf install -y zsh
+sudo dnf install -y zsh git
 sudo dnf update -y
 
 # migrate gnome setting
@@ -21,6 +21,11 @@ rm -rf ~/.oh-my-zsh
 echo "now we are installing oh-my-zsh. exit after completed. press any key to continue..." && read
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zshrc ~/
+
+# history-search-multi-word
+cd ~/.oh-my-zsh/custom/plugins
+git clone https://github.com/zdharma/history-search-multi-word.git
+cd 
 
 echo "remember to find gnome extensions in gnome.dconf.back, and install them. "
 echo "done."
