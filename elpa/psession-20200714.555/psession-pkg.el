17b1983e0e60de46594e97cd980c0fb7967480b7
Crane Chu <cranechu@gmail.com>
2020-10-04 17:01:38 +0800

    updated packages

---
+++ elpa/psession-20200714.555/psession-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/cranechu/.emacs.d/elpa/psession-20200714.555/psession.el  -*- no-byte-compile: t -*-
+(define-package "psession" "20200714.555" "Persistent save of elisp objects." '((emacs "24") (cl-lib "0.5") (async "1.9.3")) :commit "d2b8f8d8c6acd2f86f3a7774a7079f40b1b75f8e" :authors '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")) :maintainer '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com") :url "https://github.com/thierryvolpiatto/psession")
