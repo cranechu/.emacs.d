0a207e4c0cfd5e06226192b906626570c9eed603
Crane Chu <cranechu@gmail.com>
2021-01-08 13:38:10 +0800

    emacs files

--- transient/history.el
+++ transient/history.el
@@ -1,9 +1,10 @@
 ((magit-am
   ("--3way"))
  (magit-commit nil)
+ (magit-dispatch nil)
  (magit-push nil)
  (python-pytest-dispatch
   ("--color")
-  ("--color" "--capture=no" "--log-cli-level=debug")
   ("--color" "--capture=no")
+  ("--color" "--capture=no" "--log-cli-level=debug")
   ("--color" "--log-cli-level=debug")))
