695eb3de3f1ad538310134966e14e9a98bedd4dc
Crane Chu <cranechu@gmail.com>
2021-04-06 10:39:33 +0800

    update emacs packages

--- elpa/zzz-to-char-20190713.1344/zzz-to-char-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zzz-to-char" "20190713.1344" "Fancy version of `zap-to-char' command" '((emacs "24.4") (cl-lib "0.5") (avy "0.3.0")) :commit "3499777d74add95625b4592b5d675a82abc8f888" :keywords '("convenience") :authors '(("Mark Karpov" . "markkarpov92@gmail.com")) :maintainer '("Mark Karpov" . "markkarpov92@gmail.com") :url "https://github.com/mrkkrp/zzz-to-char")
