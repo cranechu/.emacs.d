b0a1ee2de03c0578bb8337ae86304f854209a45a
Crane Chu <cranechu@gmail.com>
2022-08-18 16:17:22 +0800

    install more files

--- restore.sh
+++ restore.sh
@@ -11,7 +11,7 @@ sudo apt install gnome-shell-extension-desktop-icons -y
 sudo apt remove gnome-shell-extension-appindicator -y
 sudo apt remove ghostscript firefox -y
 
-sudo apt install -y python3-dev python-is-python3
+sudo apt install -y python3-dev python-is-python3 python3-pip
 sudo apt install -y emacs
 sudo apt install -y gnome-tweak-tool
 sudo apt install -y zsh git
@@ -34,5 +34,9 @@ cp .zshrc ~/
 cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/robobenklein/zdharma-history-search-multi-word.git history-search-multi-word
 cd 
+
+# download chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+echo "double click to install chrome"
 
 echo "done."
