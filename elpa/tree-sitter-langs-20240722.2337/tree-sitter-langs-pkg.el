fd3a042ff35b63c370b6ab4fbc985561b4e8fd7c
Crane Chu <cranechu@gmail.com>
2024-07-24 08:49:07 +0800

    upgrade

---
+++ elpa/tree-sitter-langs-20240722.2337/tree-sitter-langs-pkg.el
@@ -0,0 +1,15 @@
+(define-package "tree-sitter-langs" "20240722.2337" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.15.0"))
+  :commit "0e603504b8d4f9865d149105dff619072ab0ed40" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainers
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
